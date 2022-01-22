2c3ed6de01dddfe622c9565f2aa1679a3ff5afa4
developer-kimdaehee <ocuuco@nate.com>
2022-01-23 00:32:33 +0900

    2022.01.22

--- index.php
+++ index.php
@@ -16,194 +16,255 @@
 	<body class="is-preload">
 
 		<!-- Wrapper -->
-			<div style="line-height: 100px;" id="wrapper">
-				
+			<div style="line-height: 100%;" id="wrapper">
+
 				<!-- Intro -->
-					<section  class="intro">
-						<style>
-							ul{
-							   list-style:none;
-								 padding-left:0px;
-							   }
-							</style>
+					<section class="intro">
 						<header>
-							<h1 style="font-size: 130px;">김대희 &nbsp;&nbsp;개발 &nbsp;&nbsp;&nbsp;&nbsp;노트</h1>
-							<p>Developer Kim Daehee's notebook. YouTube channel. <a href="https://www.youtube.com/channel/UCPRM9Jl_7s3PEXv1WvVYwjQ"><strong style="font-size: 22px; color: red;">-개발하는 김대희-</strong></a></p>
-
-							<pre>
-
-							</pre>
- 
-								<h1 style="font-size: 100px;">Porject List</h1>
-								<pre></pre>
-							<ul style="width: 60%;">
-								 <li style="font-size: 80px;">V-Studio<a href="#first" class="arrow scrolly"><span class="label">Next</span></a></li>
-							</ul>
-							<ul style="width: 60%;">
-								<li style="font-size: 80px;">Rush&Fish<a href="#second" class="arrow scrolly"><span class="label">Next</span></a></li>
-						    </ul>
-						    <ul style="width: 60%;">
-							    <li style="font-size: 80px;">Matador<a href="#third" class="arrow scrolly"><span class="label">Next</span></a></li>
-					        </ul>
+							<h1><pre style="font-size: 100%;">
+김대희
+개발
+노트
+<p style="font-size: 30%;">Developer Kim Daehee's note.
+ YouTube channel. <a href="https://www.youtube.com/channel/UCPRM9Jl_7s3PEXv1WvVYwjQ"><strong style="font-size: 80%; color: red;">개발하는 김대희</strong></a></p>
+							</pre> </h1>
+							<h1 style="font-size: 400%;">Porject List</h1>							
 							
+							<ul class="actions" >
+								<li style="font-size: 50px;">V-Studio<a href="#first" class="arrow scrolly"><span class="label">Next</span></a></li>
+						   </ul>
+						   <ul class="actions" >
+							   <li style="font-size: 50px;">Rush&Fish<a href="#second" class="arrow scrolly"><span class="label">Next</span></a></li>
+						   </ul>
+						   <ul class="actions" >
+							   <li style="font-size: 50px;">Matador<a href="#third" class="arrow scrolly"><span class="label">Next</span></a></li>
+						   </ul>
 						</header>
 						<div class="content">
 							<span class="image fill" data-position="center"><img src="images/Abata.png" alt="" /></span>
 						</div>
 					</section>
-					<pre>
-
-					</pre>
-
 
 				<!-- Section -->
-					<section style="line-height: 50px;" id="first">
-						<header style="width: 60%;">
-							<h2 style="font-size: 60px;"><strong>버츄얼 유튜버를 위한 XR 가상 스튜디오 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-V-Studio-</strong></h2>
-							<h2 style="font-size: 50px;"><strong>팀. Go-Gal</strong></h2>
+					<section id="newfirst">
+						<header>
+							<h2>Magna sed nullam nisl adipiscing</h2>
 						</header>
 						<div class="content">
-							<iframe width="1650" height="1000" src="https://www.youtube.com/embed/uwOWyGnF2Nc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
-							<!-- <span class="image main"><img src="images/Vstudio main picter.png" alt="" /></span> -->
-
-							
-<p><pre style="font-size: 30px;"><strong>개발배경</strong>
-    • 네트워크 환경과 하드웨어 기술력 향상으로 시ㆍ공간 제약 없이 누구나 영상 제작이 가능한 시대
-    • COVID 19로 인해 비대면 환경이 급속도로 증가하고 있으며, 연령층에 상관없이 다양한 분야로부터 
-      콘텐츠의 수요 증가
-    • 라이브 스트리밍의 특성상 편집이 불가한 환경을 대체할 방안 모색
-    • 의도치 않은 문제 발생 시 개인의 정보가 보호받지 못한 상황에서 무분별하게 
-      공유 및 확산될 위험이 있음(사생활 보호 필요성)
-</pre></p>
-
-<p><pre style="font-size: 30px;"><strong>개발목적</strong>
-    • 한계점 극복(현실감, 몰입감, 가상스튜디오) 
-    • 현실감 있는 메타휴먼 캐릭터를 선택하여 라이브방송 진행
-    • 다양한 분위기의 3D 가상스튜디오를 표현
-    • 몰입감을 위해 스튜디오 페이스 트래킹, 모션 캡처, 애니메이션, 실시간 채팅 
-      투표 기능 활용 가능
-    • 기존 서비스 대비 비용적인 절감
-</pre></p>
-                    <button onclick="PPT열기()"><strong style="color: red; font-size: 40px;">V-Studio_PPT</strong> </button>
-                            <div style="width: 1000px; height: 1250px; display: block; " id="V-Studio-PPT"> <iframe style="width: 1700px; height:1250px;" src="./Go-Galbi팀 3차 ppt 자료.pdf"></iframe>
-								<button onclick="PPT닫기()"><strong style="color: red; font-size: 40px;">Close</strong> </button>
-							</div>
-
-							<div>
-								<pre>
-	
-								</pre>
-								<h2 style="font-size: 40px;"><strong>-기능구현/개발사항-</strong></h2>
-							</div>	
+							<p><strong>Lorem ipsum dolor</strong> sit amet consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor amet fermentum. Nullam venenatis erat id vehicula ultrices sed ultricies condimentum. Magna sed etiam consequat, et lorem adipiscing sed nulla. Volutpat nisl et tempus et dolor libero, feugiat magna tempus, sed et lorem adipiscing.</p>
+							<span class="image main"><img src="images/pic02.jpg" alt="" /></span>
 						</div>
-					
-						<script>
-							function PPT열기(){
-								document.getElementById('V-Studio-PPT').style = 'block';
-							}
-							function PPT닫기(){
-								document.getElementById('V-Studio-PPT').style.display = 'none';
-							}
-							
-						</script>
 					</section>
 
 
-                <!-- Section -->
-					<section style="line-height: 50px;" id="second">
-						<header style="width: 60%;">
-							<h2 style="font-size: 60px;"><strong>귀여운 물고기들의 멈출 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;수 없는 한판 승부! -Rush&Fish- </strong></h2>
-							<h2 style="font-size: 50px;"><strong>팀. 김대희 황상욱 김소연</strong></h2>
-						</header>
-						<div class="content">
-							<iframe width="1650" height="1000" src="https://www.youtube.com/embed/RJJDhlulCss" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+									<!-- Section -->
+									<section style="line-height: 50px; height: auto;" id="first">
+										<header style="width: 100%;">
+											<h2 ><strong><pre style="font-size: 200%;">
+버츄얼 유튜버를 위한
+XR 가상 스튜디오
+-V-Studio-
+											</pre> </strong></h2>
+											<h2 style="font-size: 150%;"><strong>팀. Go-Gal</strong></h2>
+										</header>
+										<div class="content">
+											<iframe width="105%" height="30%" src="https://www.youtube.com/embed/uwOWyGnF2Nc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+				
+											<!-- <span class="image main"><img src="images/Vstudio main picter.png" alt="" /></span> -->
+				
+											
+				<p><pre style="font-size: 100%; line-height:150%; letter-spacing: 1px;"><strong style="font-size: 150%;">개발배경</strong>
+ • 네트워크 환경과 하드웨어 기술력 향상으로 시ㆍ공간 제약 없이 영상 제작이 가능한 시대
+ • COVID 19로 인해 비대면 환경이 급속도로 증가하고 있으며, 연령층에 상관없이 다양한 
+   분야로부터 콘텐츠의 수요 증가
+ • 라이브 스트리밍의 특성상 편집이 불가한 환경을 대체할 방안 모색
+ • 의도치 않은 문제 발생 시 개인의 정보가 보호받지 못한 상황에서 무분별하게 
+   공유 및 확산될 위험이 있음(사생활 보호 필요성)
+				</pre></p>
+				
+				<p><pre style="font-size: 100%; line-height:150%"><strong style="font-size: 150%;">개발목적</strong>
+ • 한계점 극복(현실감, 몰입감, 가상스튜디오) 
+ • 현실감 있는 메타휴먼 캐릭터를 선택하여 라이브방송 진행
+ • 다양한 분위기의 3D 가상스튜디오를 표현
+ • 몰입감을 위해 스튜디오 페이스 트래킹, 모션 캡처, 애니메이션, 실시간 채팅 
+   투표 기능 활용 가능
+ • 기존 서비스 대비 비용적인 절감
+				</pre></p>
+									<button onclick="PPT열기()"><strong style="color: red; font-size: 200%;">V-Studio_PPT</strong> </button>
+											<div display: block; id="V-Studio-PPT"> <iframe style="width: 800px; height:650px;" src="./Go-Galbi팀 3차 ppt 자료.pdf"></iframe>
+												<button onclick="PPT닫기()"><strong style="color: red; font-size: 200%;">Close</strong> </button>
+												<pre>
+												</pre>
+											</div>
+				
+											<div>
+												
+												<h2 style="font-size: 200%;"><strong>-기능구현/개발사항-</strong></h2>
 
-							<!-- <span class="image main"><img src="images/Vstudio main picter.png" alt="" /></span> -->
+												
+											</div>	
+										</div>
+									
+										<script>
+											function PPT열기(){
+												document.getElementById('V-Studio-PPT').style = 'block';
+											}
+											function PPT닫기(){
+												document.getElementById('V-Studio-PPT').style.display = 'none';
+											}
+											
+										</script>
+									</section>
+				
+				<pre>
 
-							
-<p><pre style="font-size: 30px;"><strong>플랫폼 모바일 게임</strong>
-    • 무한이 생성 되는 맵에서 상호 작용이 가능한 오브젝트들을 활용 하여 스코어를 획득! 
-    • 하이스코어(무한)가 저장되며 최고점수를 얻는게임.
-    • 귀엽고 아기자기한 모드별 캐릭터와 오브젝트 및 임펙트 그리고 22개의 경쾌한 사운드가 장점!
-    • 내기 게임 한판 승부~! 하지만 꼭 꼴등은..OTL
 
-</pre></p>
-
-
-                    <button onclick="PPT열기()"><strong style="color: red; font-size: 40px;">Rush&Fish</strong> </button>
-                            <div style="width: 1000px; height: 900px; display: block; " id="V-Studio-PPT"> <iframe style="width: 1700px; height:900px;" src="./Rush&Fish ppt.pdf"></iframe>
-								<button onclick="PPT닫기()"><strong style="color: red; font-size: 40px;">Close</strong> </button>
-							</div>
-
-							<div>
-								<pre>
-	
-								</pre>
-								<h2 style="font-size: 40px;"><strong>-기능구현/개발사항-</strong></h2>
-							</div>	
-						</div>
 					
-						<script>
-							function PPT열기(){
-								document.getElementById('V-Studio-PPT').style = 'block';
-							}
-							function PPT닫기(){
-								document.getElementById('V-Studio-PPT').style.display = 'none';
-							}
+
+
+
+
+
+
+
+
+
+
+				</pre>
+				
+				
+				<!-- Section -->
+				<section style="line-height: 50px; height: auto;" id="second">
+						<header style="width: 100%;">
+							<h2 ><strong><pre style="font-size: 200%;">
+귀여운 물고기들의 
+멈출 수 없는 한판 승부! 
+-Rush&Fish- 
+							</pre> </strong></h2>
+						    <h2 style="font-size: 150%;"><strong>팀. 김대희 황상욱 김소연</strong></h2>
+						</header>
+								<div class="content">
+									<iframe width="105%" height="40%" src="https://www.youtube.com/embed/RJJDhlulCss" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+									<!-- <span class="image main"><img src="images/Vstudio main picter.png" alt="" /></span> -->
+								
+															
+								<p><pre style="font-size: 100%; line-height:150%; letter-spacing: 1px;"><strong style="font-size: 150%;">개발배경</strong>
+ • 무한이 생성 되는 맵에서 상호 작용이 가능한 오브젝트들을 활용 하여 스코어를 획득! 
+ • 하이스코어(무한)가 저장되며 최고점수를 얻는게임.
+ • 귀엽고 아기자기한 모드별 캐릭터와 오브젝트 및 임펙트 그리고 22개의 경쾌한 
+   사운드가 장점!
+ • 내기 게임 한판 승부~! 하지만 꼭 꼴등은..OTL
+								</pre></p>
+								<button onclick="PPT열기()"><strong style="color: red; font-size: 200%;">Rush&Fish_PPt</strong> </button>
+								<div display: block; id="V-Studio-PPT"> <iframe style="width: 800px; height:500px;" src="./Rush&Fish ppt.pdf"></iframe>
+								<button onclick="PPT닫기()"><strong style="color: red; font-size: 200%;">Close</strong> </button>
+								<pre>
+								</pre>
+								</div>
+								
+								<div>
+																
+								<h2 style="font-size: 200%;"><strong>-기능구현/개발사항-</strong></h2>
+				
+																
+								</div>	
+														
+													
+								<script>
+									function PPT열기(){
+									document.getElementById('V-Studio-PPT').style = 'block';
+									}
+									function PPT닫기(){
+									document.getElementById('V-Studio-PPT').style.display = 'none';
+									}
+															
+									</script>
+							</section>
+								
+								<pre>
+				
+				
+									
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+								</pre>
+				
+				
+				<!-- Section -->
+				<section style="line-height: 50px; height: auto;" id="third">
+					<header style="width: 100%;">
+						<h2 ><strong><pre style="font-size: 200%;">
+호러 좀비 방탈출 게임!
+-Matador-
+						</pre> </strong></h2>
+						<h2 style="font-size: 150%;"><strong>팀. 김대희 황상욱</strong></h2>
+					</header>
+							<div class="content">
+								<iframe width="105%" height="40%" src="https://www.youtube.com/embed/21q4pN0E4vQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 							
-						</script>
-					</section>
-
-
- <!-- Section -->
- <section style="line-height: 50px;" id="third">
-	<header style="width: 60%;">
-		<h2 style="font-size: 60px;"><strong>호러 좀비 방탈출 게임! &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-Matador- </strong></h2>
-		<h2 style="font-size: 50px;"><strong>팀. 김대희 황상욱</strong></h2>
-	</header>
-	<div class="content">
-		<iframe width="1650" height="1000" src="https://www.youtube.com/embed/21q4pN0E4vQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
-		<!-- <span class="image main"><img src="images/Vstudio main picter.png" alt="" /></span> -->
-
-		
-<p><pre style="font-size: 30px;"><strong>데스크탑 게임</strong>
-• 눈을 뜨니 좀비들에게 둘러 쌓여 영혼 상태인 나... 좀비에 빙의하여 문제를 해결하고 이곳을 탈출하라!  
-• 호러한 분위기를 부각 시키는 맵 디자인과 긴장감을 고조 시키는 BGM!
-• 각기 다른 개성의 9마리 좀비와 다중의 엔딩 및 게임오버.
-# 언리얼엔진4 개발 첫 프로젝트! 처음이라 대가리 깨지며 작업 했지만 개발하는 맛을 알아버림!! 
-
-</pre></p>
-
-
-<button onclick="PPT열기()"><strong style="color: red; font-size: 40px;">Matador</strong> </button>
-		<div style="width: 1000px; height: 1600px; display: block; " id="V-Studio-PPT"> <iframe style="width: 1700px; height:1600px;" src="./Matador ppt.pdf"></iframe>
-			<button onclick="PPT닫기()"><strong style="color: red; font-size: 40px;">Close</strong> </button>
-		</div>
-
-		<div>
-			<pre>
-
-			</pre>
-			<h2 style="font-size: 40px;"><strong>-기능구현/개발사항-</strong></h2>
-		</div>	
-	</div>
-
-	<script>
-		function PPT열기(){
-			document.getElementById('V-Studio-PPT').style = 'block';
-		}
-		function PPT닫기(){
-			document.getElementById('V-Studio-PPT').style.display = 'none';
-		}
-		
-	</script>
-</section>
-
+														
+							<p><pre style="font-size: 100%; line-height:150%; letter-spacing: 1px;"><strong style="font-size: 150%;">개발배경</strong>
+ • 눈을 뜨니 좀비들에게 둘러 쌓여 영혼 상태인 나... 좀비에 빙의하여 문제를 
+   해결하고 이곳을 탈출하라!  
+ • 호러한 분위기를 부각 시키는 맵 디자인과 긴장감을 고조 시키는 BGM!
+ • 각기 다른 개성의 9마리 좀비와 다중의 엔딩 및 게임오버.
+ # 언리얼엔진4 개발 첫 프로젝트! 처음이라 대가리 깨지며 작업 했지만 개발하는
+   맛을 알아버림!! 
+							</pre></p>
+							<button onclick="PPT열기()"><strong style="color: red; font-size: 200%;">Matador_PPt</strong> </button>
+							<div display: block; id="V-Studio-PPT"> <iframe style="width: 800px; height:500px;" src="./Rush&Fish ppt.pdf"></iframe>
+							<button onclick="PPT닫기()"><strong style="color: red; font-size: 200%;">Close</strong> </button>
+							<pre>
+							</pre>
+							</div>
+							
+							<div>
+															
+							<h2 style="font-size: 200%;"><strong>-기능구현/개발사항-</strong></h2>
+			
+															
+							</div>	
+													
+												
+							<script>
+								function PPT열기(){
+								document.getElementById('V-Studio-PPT').style = 'block';
+								}
+								function PPT닫기(){
+								document.getElementById('V-Studio-PPT').style.display = 'none';
+								}
+														
+								</script>
+						</section>
+							
+							<pre>
+			
+			
+								
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+							</pre>				
+				
+				 
 
 				<!-- Section -->
-					<!-- <section id="second">
+					<!-- <section>
 						<header>
 							<h2>Feugiat consequat tempus ultrices</h2>
 						</header>
@@ -222,7 +283,7 @@
 					</section> -->
 
 				<!-- Section -->
-					<!-- <section id="third">
+					<!-- <section>
 						<header>
 							<h2>Ultrices erat magna sed condimentum</h2>
 						</header>
@@ -243,17 +304,13 @@
 											<a href="images/gallery/fulls/04.jpg" class="landscape"><img src="images/gallery/thumbs/04.jpg" alt="" /></a>
 										</div>
 									</div>
-								</section>
+								</section> -->
 
 							<!-- Section -->
 								<!-- <section>
 									<header>
 										<h3>Nisl consequat</h3>
 										<p>Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin aliquam sed facilisis ante interdum congue. Integer mollis, nisl amet convallis, porttitor magna ullamcorper, amet mauris. Ut magna finibus nisi nec lacinia ipsum maximus.</p>
-									</header><section>
-									<header>
-										<h3>Lorem gravida</h3>
-										<p>Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aenean ornare velit lacus, ac varius sed enim lorem ullamcorper dolore.  ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis.</p>
 									</header>
 									<div class="content">
 										<div class="gallery">
@@ -265,8 +322,12 @@
 								</section> -->
 
 							<!-- Section -->
-								
-									<!-- <div class="content">
+								<!-- <section>
+									<header>
+										<h3>Lorem gravida</h3>
+										<p>Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aenean ornare velit lacus, ac varius sed enim lorem ullamcorper dolore.  ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis.</p>
+									</header>
+									<div class="content">
 										<div class="gallery">
 											<a href="images/gallery/fulls/08.jpg" class="portrait"><img src="images/gallery/thumbs/08.jpg" alt="" /></a>
 											<a href="images/gallery/fulls/09.jpg" class="portrait"><img src="images/gallery/thumbs/09.jpg" alt="" /></a>
@@ -276,7 +337,7 @@
 								</section>
 
 						</div>
-					</section> --> -->
+					</section> -->
 
 				<!-- Section -->
 					<!-- <section>
@@ -559,8 +620,7 @@ print 'Sorted in ' + i + ' iterations.';</code></pre>
 						</div>
 					</section>
 				-->
-
-				<!-- Section -->
+                <!-- Section -->
 					<section>
 						<header>
 							<h2>Get in touch</h2>
@@ -601,7 +661,7 @@ print 'Sorted in ' + i + ' iterations.';</code></pre>
 								<li>
 									<h3>Elsewhere</h3>
 									<ul class="icons">
-										<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+										<li><a href="#" class="icon brands fa-youtube"><span class="label">Twitter</span></a></li>
 										<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
 										<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
 										<li><a href="#" class="icon brands fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
@@ -627,3 +687,4 @@ print 'Sorted in ' + i + ' iterations.';</code></pre>
 
 	</body>
 </html>
+				
